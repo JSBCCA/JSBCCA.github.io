c5fdead6b14fdd7e9a958852c3ecc1665fa37a43
James Sibert <jamessibert@ssa-417.local>
2016-09-06 15:04:04 -0500

    fixed health going lower than zero

--- gladiators/gladiatorjs.js
+++ gladiators/gladiatorjs.js
@@ -6,22 +6,23 @@ function heal_one() {
     alert("Can't do dat. " + health_one);
   } else if (health_one > 0) {
     health_one = health_one + 10;
-    alert("You're good, m8. " + health_one);
+    alert("You: " + health_one);
+    var health_bar = document.getElementsByClassName('progress-bar-success')[0];
+    health_bar.style.width = health_one + "%";
   } else if (health_one <= 0) {
-    alert("R.I.P. in Peace " + health_one);
+    alert("R.I.P. in Peace. " + health_one);
   }
 }
 
 function attack_one() {
   if (health_two > 0) {
-    health_two = health_two - 20;
-    alert("hyahhhhh " + health_two);
+    health_two = Math.max(health_two - 20, 0);
+    alert("Other guy: " + health_two);
   }
-
   // get the relevant element(s)
   // update it(them)
-  var health_bar = document.getElementsByClassName('progress-bar-success')[1];
-  health_bar.style.width = health_two + "%";
+    var health_bar = document.getElementsByClassName('progress-bar-success')[1];
+    health_bar.style.width = health_two + "%";
 }
 
 function heal_two() {
@@ -29,15 +30,19 @@ function heal_two() {
     alert("Can't do dat. " + health_two);
   } else if (health_two > 0) {
     health_two = health_two + 10;
-    alert("You're good, m8. " + health_two);
+    alert("You: " + health_two);
+    var health_bar = document.getElementsByClassName('progress-bar-success')[1];
+    health_bar.style.width = health_two + "%";
   } else if (health_two <= 0) {
-    alert("R.I.P. in Peace " + health_two);
+    alert("R.I.P. in Peace. " + health_two);
   }
 }
 
 function attack_two() {
   if (health_one > 0) {
-    health_one = health_one - 20;
-    alert("hyahhhhh " + health_one);
+    health_one = Math.max(health_one - 20, 0);
+    alert("Other guy: " + health_one);
   }
+    var health_bar = document.getElementsByClassName('progress-bar-success')[0];
+    health_bar.style.width = health_one + "%";
 }
