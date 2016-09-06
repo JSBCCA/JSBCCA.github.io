af520d3eed951d9cb0622713c7a055ba0041d095
James Sibert <jamessibert@ssa-417.local>
2016-09-06 13:05:04 -0500

    added health to messages

--- gladiators/gladiatorjs.js
+++ gladiators/gladiatorjs.js
@@ -1,14 +1,38 @@
-var health = 100;
+var health_one = 100;
+var health_two = 100;
 
-function heal() {
-  if (health > 0) {
-    alert("You're good, m8.");
-  } else if (health <= 0) {
-    alert("R.I.P. in Pasta");
+function heal_one() {
+  if (health_one >= 100) {
+    alert("Can't do dat. " + health_one);
+  } else if (health_one > 0) {
+    health_one = health_one + 10;
+    alert("You're good, m8. " + health_one);
+  } else if (health_one <= 0) {
+    alert("R.I.P. in Peace " + health_one);
   }
 }
 
+function attack_one() {
+  if (health_two > 0) {
+    health_two = health_two - 20;
+    alert("hyahhhhh " + health_two);
+  }
+}
 
-function attack() {
-  alert("hyahhhhh");
+function heal_two() {
+  if (health_two >= 100) {
+    alert("Can't do dat. " + health_two);
+  } else if (health_two > 0) {
+    health_two = health_two + 10;
+    alert("You're good, m8. " + health_two);
+  } else if (health_two <= 0) {
+    alert("R.I.P. in Peace " + health_two);
+  }
+}
+
+function attack_two() {
+  if (health_one > 0) {
+    health_one = health_one - 20;
+    alert("hyahhhhh " + health_one);
+  }
 }
