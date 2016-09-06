4dfad03517997d0ff8285a79f0177931a5df68ed
James Sibert <jamessibert@ssa-417.local>
2016-09-06 11:37:59 -0500

    made heal button display message based on what your health is

--- gladiators/gladiatorjs.js
+++ gladiators/gladiatorjs.js
@@ -1,1 +1,14 @@
-function something() {alert("WE DID IT!");}
+var health = 100;
+
+function rage() {
+  if (health > 0) {
+    document.write("You're good, m8.");
+  } else if (health <= 0) {
+    document.write("R.I.P. in Peace");
+  }
+}
+
+
+function something() {
+  alert("WE DID IT!");
+}
