c8e56ecd5bdc71203a75b49a5ed291c42625f7cc
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-06 11:45:25 -0500

    working on structure

--- jquery_counter/jquery_counter.js
+++ jquery_counter/jquery_counter.js
@@ -1,1 +1,12 @@
-$("")
+$(function up() {
+  $('') += 1
+});
+$(function down() {
+  $('') -= 1
+});
+var $body = $('<body>');
+$('<span>***buttonthing1***</span>').prependTo($body);
+$('<span>***number***</span>').prependTo($body);
+$('<span>***buttonthing2***</span>').prependTo($body);
+$("***button1***").on('click', up());
+$("***button2***").on('click', down())
