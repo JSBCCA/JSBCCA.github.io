c03c35193e349d1b9e8e7c0243d441a662f3a1bf
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-06 12:49:24 -0500

    structure

--- jquery_counter/jquery_counter.js
+++ jquery_counter/jquery_counter.js
@@ -1,12 +1,33 @@
-$(function up() {
-  $('') += 1
-});
-$(function down() {
-  $('') -= 1
-});
-var $body = $('<body>');
-$('<span>***buttonthing1***</span>').prependTo($body);
-$('<span>***number***</span>').prependTo($body);
-$('<span>***buttonthing2***</span>').prependTo($body);
-$("***button1***").on('click', up());
-$("***button2***").on('click', down())
+var Counter = {
+  generate_counter: function() {
+    //
+    var add_button = $("<button>+</button>");
+    var sub_button = $("<button>-</button>");
+    var count = $("<span>0</span>");
+
+    var counter = $("<div></div>");
+    counter.append(sub_button);
+    counter.append(count);
+    counter.append(add_button);
+
+    $("body").append(counter);
+  }
+}
+
+Counter.generate_counter()
+Counter.generate_counter()
+Counter.generate_counter()
+
+
+// $(function up() {
+//   $('span.num') += 1
+// });
+// $(function down() {
+//   $('span.num') -= 1
+// });
+// var $body = $('<body>');
+// $('<span><button class="plus">+</button></span>').prependTo($body);
+// $('<span class="num">0</span>').prependTo($body);
+// $('<span><button class="minus">-</button></span>').prependTo($body);
+// $("button.plus").on('click', up());
+// $("button.minus").on('click', down())
